fa06ff6c92c79386cbcce9f49b8e06b6dcf4f2b3
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-03 16:16:46 +0100

    corrected key: Annual Total2014/2015

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ for row in reader:
     row['Actual Funding Award'] = row['Actual Funding Award'].decode("latin-1")
     row['Annual Total 2012/2013'] = row['Annual Total 2012/2013'].decode("latin-1")
     row['Annual Total 2013/2014'] = row['Annual Total 2013/2014'].decode("latin-1")
-    row['Annual Total 2014/2015'] = row['Annual Total 2014/2015'].decode("latin-1")
+    row['Annual Total2014/2015'] = row['Annual Total2014/2015'].decode("latin-1")
     row['Overall Total Paid'] = row['Overall Total Paid'].decode("latin-1")
     print row['Ref. No.']
     scraperwiki.sqlite.save(['Ref. No.'], row)
