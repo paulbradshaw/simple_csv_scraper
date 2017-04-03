458714a550e689d39a98a6aa4e0c8037688427d9
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-03 16:10:25 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,11 +3,12 @@ import csv
 data = scraperwiki.scrape('https://data.birmingham.gov.uk/dataset/14492d37-1a77-4d46-9204-27363fc62149/resource/bacf38dd-3530-4c95-a0c3-83e21c9b2259/download/sgmsreportsvcsfreportsvcsfreports201415vcsfreport2014qtr1final.csv')
 
 reader = csv.DictReader(data.splitlines())
-for key, value in reader.items():
-    print key
+
 
 for row in reader:
     print row["Annual Total 2012/2013"]
+    for key, value in reader.items():
+        print key
     print "Row item 3:", row['Actual Funding Award'][2:]
     for num in range(0,8):
         print "column %d : " % num, type(row.keys()[num]) 
